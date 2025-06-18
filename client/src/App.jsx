ae26edf210411c7075110036a1ba92fa74c747c9
Sai Rithwik Bejadi <rithwikbejadi696@gmail.com>
2025-06-18 15:22:34 +0530

    modifications

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Signup from "./pages/Signup/Signup.jsx";
+import Signin from "./pages/Signin/signin.jsx";
 import { Link } from 'react-router-dom';
 
 
@@ -9,6 +10,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Signup />} />
+        <Route path="/signin" element={<Signin />} />
         {/* Other routes */}
       </Routes>
     </Router>
--- client/src/pages/Signin/signin.jsx
+++ client/src/pages/Signin/signin.jsx
@@ -4,6 +4,7 @@ import facebookIcon from '../../assets/facebook.svg';
 import appleIcon from '../../assets/apple.svg';
 import mobileIcon from '../../assets/mobile.svg';
 import './Signin.css';
+import { Link } from 'react-router-dom';
 
 const Signin = () => {
   const handleLogin = (e) => {
@@ -11,32 +12,40 @@ const Signin = () => {
   };
 
   return (
-    <div className="input-box">
-      <div className="header">
-        <h1>Login</h1>
-        <p>Enter your email and password to log in</p>
-      </div>
+    <div className="singnin-body">
+      <div className="signin-container">
+        <div className="signin-card">
+          <div className="header">
+            <h1>Login</h1>
+          </div>
 
-      <form onSubmit={handleLogin}>
-        <input type="email" placeholder="Enter your Email" className="input-field" required />
-        <br />
-        <input type="password" placeholder="Enter your password" className="input-field" required />
-        <div className="password-option">
-          <label>
-            <input type="checkbox" /> Remember Me
-          </label>
-          <a href="#">Forgot password?</a>
+          <form onSubmit={handleLogin}>
+            <input type="email" placeholder="Enter your Email" required />
+            <input type="password" placeholder="Enter your password" required />
+            <div className="password-option">
+              <label>
+                <input type="checkbox" /> Remember Me
+              </label>
+              <a href="#">Forgot password?</a>
+            </div>
+            <button type="submit" className="login-button">Log In</button>
+          </form>
+
+          <div className="separator">
+            <span>Or login with</span>
+          </div>
+
+          <div className="login-options">
+            <button className="btn"><img src={googleIcon} alt="google" width="24" /></button>
+            <button className="btn"><img src={facebookIcon} alt="facebook" width="24" /></button>
+            <button className="btn"><img src={appleIcon} alt="apple" width="24" /></button>
+            <button className="btn"><img src={mobileIcon} alt="mobile" width="24" /></button>
+          </div>
+
+          <p className="signup-link">
+            Don't have an account? <Link to="/">Create an account</Link>
+          </p>
         </div>
-        <button type="submit" className="login-button">Log In</button>
-      </form>
-
-      <div className="separator">Or login with</div>
-
-      <div className="login-options">
-        <button className="btn"><img src={googleIcon} alt="google" width="24" /></button>
-        <button className="btn"><img src={facebookIcon} alt="facebook" width="24" /></button>
-        <button className="btn"><img src={appleIcon} alt="apple" width="24" /></button>
-        <button className="btn"><img src={mobileIcon} alt="mobile" width="24" /></button>
       </div>
     </div>
   );
