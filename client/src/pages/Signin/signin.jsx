d5eeb911ad9d79e6352c13db399398a286ce31a4
dncoder14 <dhirajpandit5050@gmail.com>
2025-06-19 19:17:21 +0530

    Fixed a ui glitch in signin page

--- client/src/pages/Signin/signin.jsx
+++ client/src/pages/Signin/signin.jsx
@@ -16,7 +16,7 @@ const Signin = () => {
 
     // <div className='outer-body'>
     // <div className="input-box">
-      
+
 
     <div className="singnin-body">
       <div className="signin-container">
@@ -26,43 +26,35 @@ const Signin = () => {
           </div>
 
 
-            <form onSubmit={handleLogin}>
-              <input type="email" placeholder="Enter your Email" required />
-              <input type="password" placeholder="Enter your password" required />
-              <div className="password-option">
-                <label>
-                  <input type="checkbox" /> Remember Me
-                </label>
-                <a href="#">Forgot password?</a>
-              </div>
-              <button type="submit" className="login-button">Log In</button>
-            </form>
+          <form onSubmit={handleLogin}>
+            <input type="email" placeholder="Enter your Email" required />
+            <input type="password" placeholder="Enter your password" required />
+            <div className="password-option">
+              <label>
+                <input type="checkbox" /> Remember Me
+              </label>
+              <a href="#">Forgot password?</a>
+            </div>
+            <button type="submit" className="login-button">Log In</button>
+          </form>
 
 
           <div class="separator">Or login with</div>
-                
-            <div className="login-options">
-              <button className="btn"><img src={googleIcon} alt="google" width="24" /></button>
-              <button className="btn"><img src={facebookIcon} alt="facebook" width="24" /></button>
-              <button className="btn"><img src={appleIcon} alt="apple" width="24" /></button>
-              <button className="btn"><img src={mobileIcon} alt="mobile" width="24" /></button>
-            </div>
 
-            <p className="signup-link">
-              Don't have an account? <Link to="/">Create an account</Link>
-            </p>
+          <div className="login-options">
+            <button className="btn"><img src={googleIcon} alt="google" width="24" /></button>
+            <button className="btn"><img src={facebookIcon} alt="facebook" width="24" /></button>
+            <button className="btn"><img src={appleIcon} alt="apple" width="24" /></button>
+            <button className="btn"><img src={mobileIcon} alt="mobile" width="24" /></button>
           </div>
 
-
           <p className="signup-link">
-            Don't have an account? <Link to="/Signup">Create an account</Link>
+            Don't have an account? <Link to="/">Create an account</Link>
           </p>
-
         </div>
+
       </div>
     </div>
-    // </div>
-    // </div>
   );
 };
 
